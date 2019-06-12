1543eb130700fa0bfcec873fed19592d45787d2c
panmx <panmx@gzviewit.com>
2019-06-13 01:57:40 +0800

    原页面路径带参，右键页面刷新后不带参bug解决

--- src/components/common/redirect.vue
+++ src/components/common/redirect.vue
@@ -2,8 +2,9 @@
 // 重定向空白页面，用于刷新当前组件
 export default {
   beforeCreate() {
-    const path = this.$route.params.path
-    this.$router.replace('/' + path)
+    let path = this.$route.fullPath;
+    path = path.replace('/redirect/', '/');
+    this.$router.replace(path)
   },
   render: function(h) {
     return h() // avoid warning message
